07557a463ae110a5686ac22b4db8ce4f2fa1da00
odran <odrand@users.noreply.github.com>
2017-04-27 03:16:45 -0300

    Testes pro tcc

--- HAPT/main.py
+++ HAPT/main.py
@@ -2,8 +2,8 @@
 @author: leo
 '''
 
-# from problema import Solucao
-from metaheuristicas import GRASP_VND,VNS_VND,construcao
+from problema import Solucao,beneficio
+from metaheuristicas import GRASP_VND,VNS_VND,GRASP_VNS_VND,construcao
 from manipulaInstancia import Instancia
 import numpy as np
 import threading as th
@@ -11,74 +11,102 @@ import threading as th
 def carregaTeste(solucao,testePath = 'dataset/testeSolucao'):
     slotsMatrix = []
     with open(testePath, encoding='utf-8', newline='\n') as testeSolucao:
-        for linha in testeSolucao:  
+        for linha in testeSolucao:
             slots = linha.strip().split()
             if len(slots) > 0:
                 slots = [('VAGA',None) if s.strip()=='VAGA' else (s.strip().split(',')[0],int(s.strip().split(',')[1])-1) for s in slots]
-                slotsMatrix.append(slots)              
-    for e,turmaNome in enumerate(sorted(solucao.instancia.turmas)): 
-        for i in range(0,2):               
-            for j in range(0,5):           
-                    index = (e+1)*2 - (1+i)     
-                    index = index - 1 if i == 0 else index + 1    
+                slotsMatrix.append(slots)
+    for e,turmaNome in enumerate(sorted(solucao.instancia.turmas)):
+        for i in range(0,2):
+            for j in range(0,5):
+                    index = (e+1)*2 - (1+i)
+                    index = index - 1 if i == 0 else index + 1
                     solucao.setSlot(turmaNome, i, j, slotsMatrix[index][j])
-                    
-def drange(start, stop, step):
-    r = start
-    while r < stop:
-        yield r
-        r += step
 
-def rodarTesteAlfa(alfa):
-    #print("Iniciando Thread alfa =",alfa)
-    beneficios = []
- 
-    for i in range(4):     
 
-        print("Teste %s GRASP_VND com alfa = %s" % (i+1,alfa))
-        solucao = GRASP_VND(25, instancia, alfa)  
-        print("Terminou teste %s GRASP_VND alfa = %s" % (i+1,alfa))
+def VNS_VND_test():
+    instancia = Instancia()
 
-        beneficios.append(solucao.beneficio())
+    for i in range(100):
+        print("Teste %s VNS_VND " % (i))
 
-    print("Beneficio: %s Alfa: %s" % (np.mean(beneficios),alfa))
+        solucao = VNS_VND(10,instancia,construcao(instancia, 1))
+
+        print("\nBeneficio da solucao:",beneficio(solucao))
+        print("Solucao valida" if solucao.valida() else "Solucao invalida")
+
+        print('\n------------------------------------------------------------------------------------\n')
+
+
+def GRASP_VND_test():
+    instancia = Instancia()
+
+    melhorSolucao = (construcao(instancia, 1), -1, -1)
+
+    for alfa in [.0, .25, .50, .75, 1.0]:
+        print("Teste GRASP_VND com alfa = %s" % (alfa))
+
+        solucao = GRASP_VND(100, instancia, alfa)
+
+        if beneficio(solucao) > beneficio(melhorSolucao[0]):
+            melhorSolucao = (solucao,alfa)
+
+        print("\nBeneficio da solucao:",beneficio(solucao))
+        print("Solucao valida" if solucao.valida() else "Solucao invalida")
+
+        print('\n------------------------------------------------------------------------------------\n')
+
+    melhorSolucao[0].printSolucao()
+    print("Alfa: " + melhorSolucao[1])
+
+
+def GRASP_VNS_VND_test():
+    instancia = Instancia()
+
+    melhorSolucao = (construcao(instancia, 1), -1, -1)
+
+    for alfa in [.0, .25, .50, .75, 1.0]:
+        print("Teste GRASP_VNS_VND com alfa = %s" % (alfa))
+
+        solucao = GRASP_VNS_VND(100, instancia, alfa)
+
+        if beneficio(solucao) > beneficio(melhorSolucao[0]):
+            melhorSolucao = (solucao,alfa)
+
+        print("\nBeneficio da solucao:",beneficio(solucao))
+        print("Solucao valida" if solucao.valida() else "Solucao invalida")
+
+        print('\n------------------------------------------------------------------------------------\n')
+
+    melhorSolucao[0].printSolucao()
+    print("Alfa: " + melhorSolucao[1])
+
 
 if __name__ == '__main__':
-    instancia = Instancia()
-    
-    solucao = GRASP_VND(10,instancia,.25)     
-    solucao.printSolucao()
-
-    # solucao = VNS_VND(10,instancia,construcao(instancia, .25))     
-    # solucao.printSolucao()
-
-    # threads = []
-    # # for alfa in [.0, .25, .50, .75]:
-    # for alfa in [.1, .2, .3]:
-    # # for alfa in [.3]:
-    #     threads.append(th.Thread(target=rodarTesteAlfa, args=(alfa,)))
-    # for t in threads:
-    #     t.start()
-
-    # for t in threads:
-    #     t.join()
+    # VNS_VND_test()
+    # GRASP_VND_test()
+    GRASP_VNS_VND_test()
 
     # for d in instancia.disciplinas:
-    #     print(d)   
+    #     print(d)
     # print("\n")
-    
+
     # for t in instancia.turmas:
-    #     print(t,str(instancia.disciplinasDaTurma(t))) 
+    #     print(t,str(instancia.disciplinasDaTurma(t)))
     # print("\n")
-    
+
     # for i,t in enumerate(instancia.disciplinasDoProfessori):
     #     print(i,t)
     # print("\n")
-        
+
     # for d in instancia.disciplinas:
     #     print(d,instancia.professoresHabilitados(d))
     # print("\n")
 
-    # solucao = Solucao(instancia) 
+    # solucao = Solucao(instancia)
     # carregaTeste(solucao)
+    #
+    # solucao = VNS_VND(50,instancia,solucao)
+    # solucao.printSolucao()
+    #
     # solucao.printSolucao()
--- HAPT/metaheuristicas.py
+++ HAPT/metaheuristicas.py
@@ -9,80 +9,60 @@ def buscaLocalBestImprovement(solucaoInicial,geradorDeVizinhanca):
     for vizinho in geradorDeVizinhanca:
         if vizinho.beneficio() > melhor.beneficio():
             melhor = vizinho
-    
+
     return melhor
 
 def VND(solucaoInicial):
     vizinhancas = [gerarVizinhoTHPMD,gerarVizinhoTHPDD,gerarVizinhoTTEP,gerarVizinhoTPD]
-    
+
     solucaoCorrente = copy.deepcopy(solucaoInicial)
-    
+
     k= 0
     while k < len(vizinhancas):
-        
+
         geradorDeVizinhanca = vizinhancas[k](solucaoCorrente)
-        
+
         vizinhoCorrente = buscaLocalBestImprovement(solucaoCorrente,geradorDeVizinhanca)
-        
+
         if vizinhoCorrente.beneficio() > solucaoCorrente.beneficio():
             solucaoCorrente = vizinhoCorrente
             k = 0
         else:
             k+= 1
-    
-    return solucaoCorrente
 
-def GRASP_VND(MAX_ITERACOES,instancia,alfa):
-    melhorSolucao = Solucao(instancia)
-    
-    for k in range(MAX_ITERACOES):
-        print(" GRASP: Iteracao %s"% (k+1))
-        
-        solucaoCorrente = construcao(instancia,alfa)
-        
-        # print("\n    Beneficio apos construcao:", solucaoCorrente.beneficio())
-        
-        solucaoCorrente = VNS_VND(5,instancia,solucaoCorrente)
-        
-#         print("\n    Beneficio apos VND:", solucaoCorrente.beneficio())
-        
-        if melhorSolucao.beneficio() < solucaoCorrente.beneficio():
-            melhorSolucao = copy.deepcopy(solucaoCorrente)
-            
-    return melhorSolucao  
+    return solucaoCorrente
 
 def construcao(instancia,alfa):
 #     print("    Construindo solucao com alfa = %s"%str(alfa))
-    
+
     solucao = Solucao(instancia)
     turmaDisciplinaToCargaDisponivelDic = solucao.instancia.turmaDisciplinaToCargaDic
-    
+
     while not solucao.completa():
-    
-        geradorDeCandidatos = gerarCandidatos(solucao,turmaDisciplinaToCargaDisponivelDic)   
-        todosCandidatos = [(slot,solucao.custoIncremental(slot)[1]) for slot in geradorDeCandidatos]   
-        
+
+        geradorDeCandidatos = gerarCandidatos(solucao,turmaDisciplinaToCargaDisponivelDic)
+        todosCandidatos = [(slot,solucao.custoIncremental(slot)[1]) for slot in geradorDeCandidatos]
+
         if todosCandidatos:
             todosCandidatos.sort(key=lambda x: x[1], reverse=True)
-         
-            RCL = [ c for c in todosCandidatos if custo(c) >= \
-                   custo(todosCandidatos[0]) - alfa*(custo(todosCandidatos[0]) - custo(todosCandidatos[-1])) ]
-                
+
+            RCL = [ c for c in todosCandidatos if custo(c) >= custo(todosCandidatos[0]) - alfa*(custo(todosCandidatos[0]) - custo(todosCandidatos[-1])) ]
+
             aleatorio = random.choice(RCL)[0]
-    
+
             (turmaNome,i,j) = solucao.slotVazio()
-            turmaDisciplinaToCargaDisponivelDic[turmaNome,aleatorio[0]]-= 1   
+            turmaDisciplinaToCargaDisponivelDic[turmaNome,aleatorio[0]]-= 1
             solucao.adicionar(aleatorio)
-            
+
         else:
             (turmaNome,i,j) = solucao.slotVazio()
 #             print("    Reiniciando construcao: Nenhum candidato para o slot (%d,%d) da turma %s." % (i,j,turmaNome))
 
             solucao = Solucao(instancia)
             turmaDisciplinaToCargaDisponivelDic = solucao.instancia.turmaDisciplinaToCargaDic
-  
+
     return solucao
-            
+
 def tupla(candidato):
     return candidato[0]
 
@@ -91,47 +71,85 @@ def custo(candidato):
 
 def gerarCandidatos(solucao,turmaDisciplinaToCargaDisponivelDic):
     (turmaNome,i,j) = solucao.slotVazio()
-    
+
     turmaDisciplinaToCargaDisponivelDic = copy.deepcopy(turmaDisciplinaToCargaDisponivelDic)
-    
-    for disciplina in solucao.instancia.disciplinasDaTurma(turmaNome):  
+
+    for disciplina in solucao.instancia.disciplinasDaTurma(turmaNome):
         if turmaDisciplinaToCargaDisponivelDic[turmaNome,disciplina[0]] > 0:
-            
+
             for professor in solucao.instancia.professoresHabilitados(disciplina):
- 
+
                 slot = (disciplina[0],professor)
-                    
+
                 s = copy.deepcopy(solucao)
                 s.setSlot(turmaNome,i,j,slot)
-                    
-                if s.restricaoA() and s.restricaoD():     
-                    turmaDisciplinaToCargaDisponivelDic[turmaNome,disciplina[0]]-= 1     
+
+                if s.restricaoA() and s.restricaoD():
+                    turmaDisciplinaToCargaDisponivelDic[turmaNome,disciplina[0]]-= 1
                     yield slot
+
+def GRASP_VND(MAX_ITERACOES,instancia,alfa):
+    melhorSolucao = Solucao(instancia)
+
+    for k in range(MAX_ITERACOES):
+        # print(" GRASP: Iteracao %s"% (k+1))
+
+        solucaoCorrente = construcao(instancia,alfa)
+
+        # print("\n    Beneficio apos construcao:", solucaoCorrente.beneficio())
+
+        solucaoCorrente = VND(solucaoCorrente)
+
+#         print("\n    Beneficio apos VND:", solucaoCorrente.beneficio())
+
+        if melhorSolucao.beneficio() < solucaoCorrente.beneficio():
+            melhorSolucao = copy.deepcopy(solucaoCorrente)
+
+    return melhorSolucao
+
+def GRASP_VNS_VND(MAX_ITERACOES,instancia,alfa):
+    melhorSolucao = Solucao(instancia)
+
+    for k in range(MAX_ITERACOES):
+        # print(" GRASP: Iteracao %s"% (k+1))
+
+        solucaoCorrente = construcao(instancia,alfa)
+
+        # print("\n    Beneficio apos construcao:", solucaoCorrente.beneficio())
+
+        solucaoCorrente = VNS_VND(10,instancia,solucaoCorrente)
+
+#         print("\n    Beneficio apos VND:", solucaoCorrente.beneficio())
+
+        if melhorSolucao.beneficio() < solucaoCorrente.beneficio():
+            melhorSolucao = copy.deepcopy(solucaoCorrente)
+
+    return melhorSolucao
 
 def VNS_VND(MAX_ITERACOES_SEM_MELHORA,instancia,solucao):
     vizinhancas = [gerarVizinhoTHPMD,gerarVizinhoTHPDD,gerarVizinhoTTEP,gerarVizinhoTPD]
-     
+
     # solucao = construcao(instancia, 1)
     # print("\n    Custo da solucao aleatoria inicial:",solucao.beneficio())
-     
+
     i = 0
     while i < MAX_ITERACOES_SEM_MELHORA:
         # print("\nVNS: Iteracao %s"% i)
 
         solucaoAnterior = solucao
-        
+
         k= 0
         while k < len(vizinhancas):
-            
+
             geradorDeVizinhanca = vizinhancas[k](solucao)
-            todosVizinhos = [vizinho for vizinho in geradorDeVizinhanca]           
-             
+            todosVizinhos = [vizinho for vizinho in geradorDeVizinhanca]
+
             vizinhoAleatorio = random.choice(todosVizinhos)
             # print("\n    Custo do vizinho aleatorio na vizinhanca %s: %s" % (k, vizinhoAleatorio.beneficio()))
-             
+
             vizinhoVND = VND(vizinhoAleatorio)
             # print("\n    Custo apos VND:", vizinhoVND.beneficio())
-             
+
             if vizinhoVND.beneficio() > solucao.beneficio():
                 solucao = vizinhoVND
                 k = 0
@@ -143,5 +161,5 @@ def VNS_VND(MAX_ITERACOES_SEM_MELHORA,instancia,solucao):
         else:
             i = 0
             solucaoAnterior = solucao
-            
+
     return solucao
